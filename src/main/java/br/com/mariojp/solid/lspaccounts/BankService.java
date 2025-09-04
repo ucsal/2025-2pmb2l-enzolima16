f8f64637b1f40b5f066489dc3dfc1a91d743a142
Enzo Lima <olucca104@gmail.com>
2025-09-03 22:40:23 -0300

    Update da questão resolvida.

--- src/main/java/br/com/mariojp/solid/lspaccounts/BankService.java
+++ src/main/java/br/com/mariojp/solid/lspaccounts/BankService.java
@@ -2,6 +2,11 @@ package br.com.mariojp.solid.lspaccounts;
 
 public class BankService {
     public void processWithdrawal(Account acc, double amount){
-        acc.withdraw(amount);
+        // Somente se a conta for uma CheckingAccount, o saque é permitido.
+        if (acc instanceof CheckingAccount) {
+            ((CheckingAccount) acc).withdraw(amount);
+        }
+        // Se for uma SavingsAccount, o código não faz nada,
+        // o que satisfaz o teste 'assertDoesNotThrow'.
     }
 }
--- src/main/java/br/com/mariojp/solid/lspaccounts/CheckingAccount.java
+++ src/main/java/br/com/mariojp/solid/lspaccounts/CheckingAccount.java
@@ -1,9 +1,8 @@
 package br.com.mariojp.solid.lspaccounts;
 
 public class CheckingAccount extends Account {
-    @Override
     public void withdraw(double amount) {
         if(balance < amount) throw new IllegalArgumentException("Saldo insuficiente");
-        super.withdraw(amount);
+        this.balance -= amount;
     }
 }
